912d1b337a18f8a34aa9f64c0cf93ed7cdcc809f
yangttaaoo <1285091073@qq.com>
2019-03-19 16:07:47 +0800

    ss

---
+++ SpringBoot-Test/src/main/java/com/yt/SpringBootTest/test/Test.java
@@ -0,0 +1,14 @@
+package com.yt.SpringBootTest.test;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+	@Scheduled
+	public void myTask() {
+		
+	}
+}
